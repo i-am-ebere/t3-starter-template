06aa403e3996c8a390ee4a06044a7d6eb2c7a746
kleneway <kleneway@gmail.com>
2024-02-02 19:03:22 -0800

    fixed stories

--- src/components/Login.stories.tsx
+++ src/components/Login.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { type Meta, type StoryObj } from "@storybook/react";
 import Login from "./Login";
 
 const meta: Meta = {
@@ -14,14 +14,4 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {},
-};
-
-export const WithError: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    await userEvent.type(canvas.getByLabelText(/email/i), "user@example.com");
-    await userEvent.type(canvas.getByLabelText(/password/i), "password");
-    await userEvent.click(canvas.getByRole("button", { name: /sign in/i }));
-  },
 };
